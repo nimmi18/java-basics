7631d200f27acfa6c1feedede6234056c769ec2f
nimmi18 <155964020+nimmi18@users.noreply.github.com>
2026-05-05 21:16:53 +0530

    Update and rename loop.java to if-else-elseif.java

---
+++ if-else-elseif.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main (String[]args);
+
+    Scanner sc= new Scanner(System.in);
+
+    System.out.println("enter your age");
+int age= sc.nextInt();
+  if(age>=50){
+      System.out.println("old");
+  }else if (age>=20){
+      System.out.println("adult");
+  }else{
+      System.out.println("minor"):
+      }
+  }
+}
