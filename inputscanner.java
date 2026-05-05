04f0587259377c2829d171c1631235633f373444
nimmi18 <155964020+nimmi18@users.noreply.github.com>
2026-05-05 20:02:20 +0530

    Create inputscanner.java

---
+++ inputscanner.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+  public class Main{
+    public static void main (String[]args){
+    Scanner sc = new Scanner(System.in);
+     System.out.println("enter your name");
+    String name = sc.nextLine();
+      System.out.println("Enter your age");
+    int age = sc.nextInt();
+
+      System.out.println(name+ "is"+ age+ "years old");
+  }
+}
+      
+  
